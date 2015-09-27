0f9c4d5a00de5a2e6ece6a7c83032ce1675de056
meitar <meitar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-27 03:08:07 +0000

    Pass along the username to the responder in the mockup demos.
    git-svn-id: https://plugins.svn.wordpress.org/buoy/trunk@1254176 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pages/activate-alert.php
+++ pages/activate-alert.php
@@ -1,1 +1,2 @@
-<iframe src="<?php print plugin_dir_url(__FILE__)?>../htmlmock/activation-screen/" width="100%" height="100%"><?php esc_html_e('This page requres iFrames. :(', 'better-angels');?></iframe>
+<?php $user = wp_get_current_user();?>
+<iframe src="<?php print plugin_dir_url(__FILE__)?>../htmlmock/activation-screen/?name=<?php esc_attr_e($user->display_name);?>" width="100%" height="100%"><?php esc_html_e('This page requres iFrames. :(', 'better-angels');?></iframe>
