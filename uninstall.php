be23e1d53eb268f036bab735fc9defb2f9cf5f12
meitar <meitar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-10 05:25:02 +0000

    Delete all incidents and settings upon uninstallation.
    git-svn-id: https://plugins.svn.wordpress.org/buoy/trunk@1262954 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -9,7 +9,15 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) { exit(); }
 
 // Delete options.
-//delete_option('better-angels_');
+delete_option('better-angels_settings');
+
+// Delete all incidents.
+$posts = get_posts(array(
+    'post_type' => 'better_angels_alert'
+));
+foreach ($posts as $post) {
+    wp_delete_post($post->ID, true);
+}
 
 foreach (get_users() as $usr) {
     delete_user_meta($usr->ID, 'better-angels_call_for_help');
